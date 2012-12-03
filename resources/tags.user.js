e0673a15a490eb55a16d8580e6516bf6cd344fe0
Adam Juraszek <juriad@gmail.com>
2012-12-03 14:17:07 +0100

    missing slash

--- resources/tags.user.js
+++ resources/tags.user.js
@@ -20,6 +20,6 @@ function addJS(head, file) {
 	head.appendChild(script);
 }
 
-var prefix = 'http://juriad.github.com/DJPW-tags/resources';
+var prefix = 'http://juriad.github.com/DJPW-tags/resources/';
 addCSS(head, prefix + 'tags.css');
 addJS(head, prefix + 'combined.js');
